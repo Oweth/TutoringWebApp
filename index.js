a8139d75c36d8dfdb00ce9ce3ae9321f102b4b2b
Owethu <2459030@students.wits.ac.za>
2024-09-07 16:34:31 +0200

    I added new files under Views and edited index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // index.js
+//import routes
+//const userRouter = require('./UserR.js');
 
 const express = require('express');
 const path = require('path');
@@ -7,13 +9,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'css' and 'js' directories
-app.use(express.static('css'));
-app.use(express.static('js'));
+app.use(express.static('views')); 
 
 // Serve the index.html file from the 'views' directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
+
+app.get('/tutor_dashboard.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'tutor_dashboard.html'));
+});
+
+//Use users Router
+//app.use( userRouter);
+
 
 // Start the server
 app.listen(PORT, () => {
